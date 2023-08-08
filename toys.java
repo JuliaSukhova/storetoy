8408ba30902f27015d9b4cc9d22ceca9715ecd9f
Julia Sukhova <suxova1998@gmail.com>
2023-08-08 18:22:39 +0300

    3 блок

--- toys.java
+++ toys.java
@@ -38,3 +38,13 @@ class Toy {
     private String id;
     private String name;
     private int frequency;
+
+    public Toy(String id, String name, int frequency) {
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    public String getName() {
+        return name;
+    }
